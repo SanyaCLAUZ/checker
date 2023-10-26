ddb7ffb888b032ba0a514a66e550c97480e411c4
Sasha <lendiel2002@gmail.com>
2023-10-26 15:28:46 -0400

    Login and Register views

---
+++ android/app/src/main/kotlin/ua/lendi/checker/MainActivity.kt
@@ -0,0 +1,6 @@
+package ua.lendi.checker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:checker/firebase_options.dart';
+import 'package:checker/views/login_view.dart';
+import 'package:checker/views/register_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print("You are verifed");
+              } else {
+                print("You need to verify your e-mail first");
+              }
+              return const Text("Done");
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
+}
