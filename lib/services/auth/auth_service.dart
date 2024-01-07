7c287c4ead4ac1847249a9a17fd6507ed5154f82
Sasha <lendiel2002@gmail.com>
2024-01-06 19:13:01 -0500

    To import for Ira

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,36 @@
+import 'auth_user.dart';
+import 'auth_provider.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+  const AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser(
+      {required String email, required String password}) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+}
